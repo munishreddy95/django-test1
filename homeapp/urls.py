54f3a62c1ae4d28b5a72584fb2956e735b935dda
munishreddy95 <munishkmr28@gmail.com>
2020-05-09 17:51:14 +0530

    homeapp changes lastone

--- homeapp/urls.py
+++ homeapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index,name="homepage"),
-    path('add',views.addarticle,name="addpage"),
-    path('edit/<articleid>',views.editarticle,name="editpage"),
+    path('/',views.index,name="homepage"),
+    path('add/',views.addarticle,name="addpage"),
+    path('edit/<articleid>/',views.editarticle,name="editpage"),
 ]
 
