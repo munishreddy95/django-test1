0fcd8fa2ef3f882cf25fe7d5729a6d6d969ada8f
munishreddy95 <munishkmr28@gmail.com>
2020-05-09 17:54:13 +0530

    homeapp changes finalurltesting

--- homeapp/urls.py
+++ homeapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('/',views.index,name="homepage"),
+    path('',views.index,name="homepage"),
     path('add/',views.addarticle,name="addpage"),
     path('edit/<articleid>/',views.editarticle,name="editpage"),
 ]
