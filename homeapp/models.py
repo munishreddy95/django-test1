434b5405e5da2e6e75dd64b4d96210f8c71fb91a
munishreddy95 <munishkmr28@gmail.com>
2020-05-11 14:43:49 +0530

    change to local time zone

--- homeapp/models.py
+++ homeapp/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from datetime import datetime
+from django.utils import timezone
 # Create your models here.
 
 class Article(models.Model):
     title = models.CharField(max_length=150)
     description = models.TextField()
-    created_at = models.DateTimeField(default=datetime.now())
+    created_at = models.DateTimeField(default=timezone.now)
     articleimage = models.ImageField(upload_to='articleimages',default='')
     
     def __str__(self):
